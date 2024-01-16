073919e3c47c5cb7904f2aae64ed343631b743c5
Needless01 <129036629+Needless01@users.noreply.github.com>
2024-01-17 00:00:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function moverBoton() {
     button.style.left = newX + "px";
     button.style.top = newY + "px";
 }
+
+var contadorNo = 0;
+
+function mostrarMensaje() {
+    alert("Te amo");
+}
+
+function moverBoton() {
+    contadorNo++;
+
+    if (contadorNo < 5) {
+        var button = document.getElementById("noButton");
+        var newX = Math.random() * (window.innerWidth - button.offsetWidth);
+        var newY = Math.random() * (window.innerHeight - button.offsetHeight);
+
+        button.style.left = newX + "px";
+        button.style.top = newY + "px";
+    } else {
+        alert("Sabes que la única opción es el 'Sí'");
+    }
+}
